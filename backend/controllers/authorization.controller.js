f44f4d217f223aa39fbf61deea34f14fa0121599
İbrahim Kerem Gümüş <i.keremgumus@hotmail.com>
2023-05-08 01:05:58 +0300

    added BaseResponse Model to controller

--- backend/controllers/authorization.controller.js
+++ backend/controllers/authorization.controller.js
@@ -2,6 +2,7 @@ const jwtSecret = require('../common/config/env.config').jwt_secret,
     jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const uuid = require('uuid');
+const BaseResponse = require('../models/baseresponse.model');
 
 exports.login = (req, res) => {
     try {
@@ -12,9 +13,11 @@ exports.login = (req, res) => {
         let token = jwt.sign(req.body, jwtSecret);
         let b = Buffer.from(hash);
         let refresh_token = b.toString('base64');
-        res.status(201).send({accessToken: token, refreshToken: refresh_token});
+        const response = BaseResponse.success({accessToken: token, refreshToken: refresh_token});
+        res.status(201).send(response);
     } catch (err) {
-        res.status(500).send({errors: err});
+        const response = BaseResponse.error(500, err.message, err);
+        res.status(500).send(response);
     }
 };
 
@@ -22,8 +25,10 @@ exports.refresh_token = (req, res) => {
     try {
         req.body = req.jwt;
         let token = jwt.sign(req.body, jwtSecret);
-        res.status(201).send({id: token});
+        const response = BaseResponse.success({id: token});
+        res.status(201).send(response);
     } catch (err) {
-        res.status(500).send({errors: err});
+        const response = BaseResponse.error(500, err.message, err);
+        res.status(500).send(response);
     }
 };
